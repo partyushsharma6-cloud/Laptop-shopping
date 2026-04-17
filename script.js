038b4fd2a2d0982ba8f6b1ee75f97d8b8e56ab46
partyushsharma6-cloud <partyushsharma6@gmail.com>
2026-04-17 14:05:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// 🔗 Supabase config
-const SUPABASE_URL = "https://xopxvrmmzanowgpyvolv.supabase.co";
-const SUPABASE_KEY = "sb_publishable_iODXIxkPjoMOMXel01oFDg_5Q7IvoQP";
+const supabase = window.supabase.createClient(
+  "https://xopxvrmmzanowgpyvolv.supabase.co",
+  "sb_publishable_iODXIxkPjoMOMXel01oFDg_5Q7IvoQP"
+);
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
